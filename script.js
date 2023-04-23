4a1913e7adae3aa5ee1455c0326808b091dcc742
Guilherme <devguicordeiro@gmail.com>
2023-04-22 21:48:31 -0300

    app functional. addded window icon

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ listicon.addEventListener("click", function () {
 userInput.addEventListener("keydown", function (event) {
   if (event.key == "Enter") addItem();
 });
+
+function addItem() {
+  let h2 = document.createElement("h2");
+  h2.innerHTML = "- " + userInput.value;
+
+  h2.addEventListener("click", function () {
+    h2.style.textDecoration = "line-through";
+  });
+
+  items.insertAdjacentElement("beforeend", h2);
+
+  userInput.value = "";
+}
